ec16531992d8e6bc0a3dfbe157fcfe6835ec8ff2
Ari <amt.viitala@gmail.com>
2020-04-29 19:20:12 +0300

    First part of mongo exercise done

--- mongo.js
+++ mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+
 if (process.argv.length<3) {
     console.log('give password as argument')
     process.exit(1)
@@ -11,21 +12,32 @@ if (process.argv.length<3) {
 
   mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   
-  const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String 
   })
-  
-  const Note = mongoose.model('Note', noteSchema)
-  
-  const note = new Note({
-    content: 'HTML is Easy',
-    date: new Date(),
-    important: true,
-  })
-  
-  note.save().then(response => {
-    console.log('note saved!')
-    mongoose.connection.close()
-  })
+
+  const Person = mongoose.model('Person', personSchema)
+
+  if (process.argv.length == 5){
+    const person = new Person({
+      name: process.argv[3],
+      number: process.argv[4]
+    })
+    person.save().then(response => {
+      console.log(`Added ${person.name} number ${person.number} to phonebook`)
+      mongoose.connection.close()
+    })
+  } else if (process.argv.length == 3) {
+
+    Person.find({}).then(result => {
+      console.log("phonebook:")
+      result.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
+
+  } else {
+    console.log("Wrong number of arguments")
+  }
