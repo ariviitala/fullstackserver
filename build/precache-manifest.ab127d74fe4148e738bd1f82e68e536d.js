48cad50797eda4b5d21df73b9dbcd73f2523a4f3
Ari Viitala <amt.viitala@gmail.com>
2020-04-29 15:47:36 +0300

    Frontend added

---
+++ build/precache-manifest.ab127d74fe4148e738bd1f82e68e536d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38909dcea7c4db701ac7b03a55338ddf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dedc57f8d7880db04bb5",
+    "url": "/static/css/main.c62829e1.chunk.css"
+  },
+  {
+    "revision": "3aa6ad8e86d3e13c3a81",
+    "url": "/static/js/2.ab66f117.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab66f117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dedc57f8d7880db04bb5",
+    "url": "/static/js/main.12ef8c58.chunk.js"
+  },
+  {
+    "revision": "d00a55e58aad67ff89c6",
+    "url": "/static/js/runtime-main.01dd18a1.js"
+  }
+]);
